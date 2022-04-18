4bed38042cc13aa6c66c34a148e890c1e9a8a893
MoulikaPy <103729169+MoulikaPy@users.noreply.github.com>
2022-04-18 18:18:20 +0530

    Create CatsandMouse.py

---
+++ CatsandMouse.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import os
+import sys
+
+
+#
+# Complete the catAndMouse function below.
+#
+def catAndMouse(x, y, z):
+    #
+    # Write your code here.
+    #
+    catA=abs(x-z)
+    catB=abs(y-z)
+    if catA==catB:
+        return "Mouse C"
+    elif catA<catB:
+        return "Cat A"
+    else:
+        return "Cat B"
+
+if __name__ == '__main__':
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        xyz = input().split()
+
+        x = int(xyz[0])
+
+        y = int(xyz[1])
+
+        z = int(xyz[2])
+
+        result = catAndMouse(x, y, z)
+
+        f.write(result + '\n')
+
+    f.close()
